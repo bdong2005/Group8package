b327b95556159d6515d33843f4e60212f1ceb698
Brian Dong <bdong2005@sdsu.edu>
2022-12-01 12:13:03 -0800

    Package

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' San Diego county Covid Data set
+#'
+#' An example for Group 8 R final.
+#' This is a modified version of San Diego counties Covid dataset from 2000-2022
+#' fixed for missing values and formatted dates
+#'
+#' @format A data frame with 499 obs. of 22 variables
+#' @source Original data from <https://data.sandiegocounty.gov/dataset/COVID-19-Statistics-San-Diego-County/uvug-znjd/data>
+"covid_data"
